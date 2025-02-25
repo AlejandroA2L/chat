5dd3c36b02aa1bcad17b1feef8db6b3df8f1c448
Alejandro Alas <alejandroalaslinares@gmail.com>
2025-02-24 23:23:37 -0600

    Feat(Send Message):Added function to send messages to the other user

---
+++ lib/presentation/providers/chat_provider.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:yes_no_app/domain/entities/message.dart';
+
+class ChatProvider extends ChangeNotifier {
+  List<Message> messageList = [
+    Message(text: 'Hola anonimo', fromWho: FromWho.me),
+    Message(text: 'Hola usuario', fromWho: FromWho.other),
+  ];
+
+  Future<void> sendMessage(String text) async {
+    final newMessage = Message(text: text, fromWho: FromWho.me);
+    messageList.add(newMessage);
+
+    notifyListeners();
+  }
+}
